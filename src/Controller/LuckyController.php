bd75feaecb7ed7f8df72e3b3d20b1c91c776e722
Guillermo Vidal Aldarias <gvidald774@g.educaand.es>
2022-02-08 13:52:45 +0100

    Instalación de dependencias varias

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,13 +10,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class LuckyController extends AbstractController
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number/{max}")
      */
-    public function number(): Response
+    public function number($max = 100): Response
     {
-        $number = random_int(0, 100);
+        $number = random_int(0, $max);
 
         return $this->render('lucky/number.html.twig',
         ['number' => $number,]);
     }
+
+    /**
+     * @Route("/show/{age}", name="show_age", requirements={"age"="\d+"})
+     */
+    public function age(int $age): Response
+    {
+        return new Response("Usted tiene ".$age." años");
+    }
+    
+    /**
+     * @Route("/show/{name}", name="show_name")
+     */
+    public function name(string $name = "a sí mismo"): Response
+    {
+        return new Response("Usted se llama ".$name);
+    }
+
+    
 }
