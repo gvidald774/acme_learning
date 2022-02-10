4f2498b531ea4c54ea4ddd8e92c4bf4edce5e4ec
= <gsva0001@red.ujaen.es>
2022-02-10 23:18:15 +0100

    Cambios en la dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -9,6 +9,13 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use App\Entity\Aula;
+use App\Entity\Curso;
+use App\Entity\Grupo;
+use App\Entity\Plaza;
+use App\Entity\Reclamacion;
+use App\Entity\Reserva;
+use App\Entity\Tramo;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -46,6 +53,13 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoRoute('Back to the website', 'fas fa-home', 'home');
-        yield MenuItem::linktoCrud('Usuarios', 'fas fa-solid fa-users', User::class);
+        yield MenuItem::linktoCrud('Usuarios', 'fas fa-solid fa-user', User::class);
+        yield MenuItem::linktoCrud('Aulas', 'fas fa-solid fa-school', Aula::class);
+        yield MenuItem::linktoCrud('Cursos', 'fas fa-solid fa-graduation-cap', Curso::class);
+        yield MenuItem::linktoCrud('Grupos', 'fas fa-solid fa-users', Grupo::class);
+        yield MenuItem::linktoCrud('Plazas', 'fas fa-solid fa-user-graduate', Plaza::class);
+        yield MenuItem::linktoCrud('Reclamaciones', 'fas fa-solid fa-pray', Reclamacion::class);
+        yield MenuItem::linktoCrud('Reservas', 'fas fa-solid fa-chalkboard', Reserva::class);
+        yield MenuItem::linktoCrud('Tramos', 'fas fa-solid fa-clock', Tramo::class);
     }
 }
