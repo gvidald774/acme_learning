3a364716ef368aa44defda0f1d1e1049a9f1a134
Guillermo Vidal Aldarias <gvidald774@g.educaand.es>
2022-02-15 13:55:48 +0100

    Comenzado catálogo de cursos pero vamos esto no va a estar

---
+++ public/js/carga_cursos.js
@@ -0,0 +1,8 @@
+$(function()
+{
+    alert("Vamos a ver esos cursos");
+
+    // Y si hago un embed o algo y a tomar por culo...
+    // Eso ya lo tengo que ir viendo y gestionando
+
+})
--- src/Controller/CatalogoCursosController.php
+++ src/Controller/CatalogoCursosController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CursoRepository;
 
 class CatalogoCursosController extends AbstractController
 {
-    #[Route('/catalogo/cursos', name: 'catalogo_cursos')]
+    #[Route('/cursos', name: 'catalogo_cursos')]
     public function index(): Response
     {
-        return $this->render('catalogo_cursos/index.html.twig', [
-            'controller_name' => 'CatalogoCursosController',
-        ]);
+        return $this->render('catalogo_cursos/index.html.twig');
+    }
+
+    #[Route('/cursos/{id}', name: 'detalles_curso')]
+    public function detalles(int $id): Response
+    {
+        // Id curso y sacar el curso a partir de ahí... sí.
+    }
+
+    #[Route(name: 'trae_cursos')]
+    public function traeteCursos(CursoRepository $cursos_repo): Response
+    {
+        $cursos = $cursos_repo->findAll();
+        return new Response($cursos);
     }
 }
