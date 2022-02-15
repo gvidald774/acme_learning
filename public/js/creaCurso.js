c9d4c1a938d251aaa8900d469ad9b3e70e2a1869
Guillermo Vidal Aldarias <gvidald774@g.educaand.es>
2022-02-15 10:01:58 +0100

    Añadida librería para responsive image map que no funciona, EVIDENTEMENTE

--- public/js/creaCurso.js
+++ public/js/creaCurso.js
@@ -1,31 +1,33 @@
 $(function()
 {
-    $("#pagina1").hide();
-    $("#pagina2").hide();
-    $("#pagina3").hide();
+    $('img[usemap]').rwdImageMaps();
+    
+    $.fn.activar = function()
+    {
+        this.toggleClass("activado");
+        return this;
+    }
+
+
+    muestraPagina(1);
+    $("#pagination1").activar();
 
     // Añadir función que al cambiar de página guarde los valores aquí en memoria o algo para cuestiones de relevancia e importancia
     $("#pagination1").on("click", function(ev)
     {
         ev.preventDefault();
-        $("#pagina1").show();
-        $("#pagina2").hide();
-        $("#pagina3").hide();
+        muestraPagina(1);
     })
     $("#pagination2").on("click", function(ev)
     {
         ev.preventDefault();
-        $("#pagina1").hide();
-        $("#pagina2").show();
-        $("#pagina3").hide();
+        muestraPagina(2);
 
     })
     $("#pagination3").on("click", function(ev)
     {
         ev.preventDefault();
-        $("#pagina1").hide();
-        $("#pagina2").hide();
-        $("#pagina3").show();
+        muestraPagina(3);
 
     })
 
@@ -35,4 +37,23 @@ $(function()
 
     // Ah y que no te deje pasar la página hasta que los valores estén correctos
     // (fechas en su sitio, todo escrito, etcétera)
+
+    $("button#a_pag2").on("click",function(ev) {
+        ev.preventDefault();
+        muestraPagina(2);
+        $("#pagination2").activar();
+    })
+
+    $("button#a_pag3").on("click",function(ev) {
+        ev.preventDefault();
+        muestraPagina(3);
+        $("#pagination3").activar();
+    })
+
+    function muestraPagina(id)
+    {
+        $("div[id^=pagina]").hide();
+        $("#pagina"+id).show();
+    }
+
 })
